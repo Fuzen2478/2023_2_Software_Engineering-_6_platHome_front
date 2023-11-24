067c60cae5cabe050062c533398e4846d911d84f
fuzen2478 <fuzen2478@gmail.com>
2023-11-24 19:45:22 +0900

    update FilterOption

--- app/_component/fortest/test_dummy.ts
+++ app/_component/fortest/test_dummy.ts
@@ -84,3 +84,61 @@ export const TestChat: IChat[] = [
     senderId: 1,
   },
 ];
+
+export interface FilterOption {
+  Area: {
+    gwanggyo: boolean;
+    ingyedong: boolean;
+    uman: boolean;
+    woncheon: boolean;
+    maetan: boolean;
+  };
+  RoomType: {
+    studio: boolean;
+    "two-threeRoom": boolean;
+    officetel: boolean;
+    apartment: boolean;
+  };
+  RentType: { monthly: boolean; jeonse: boolean };
+  Deposit: number;
+  MonthlyRent: number;
+  MaintenanceFee: number;
+  RoomSize: number;
+  floor: {
+    first: boolean;
+    second: boolean;
+    third: boolean;
+    fourth: boolean;
+    fifth: boolean;
+    sixth: boolean;
+    seventhUpper: boolean;
+    top: boolean; //옥탑방
+    under: boolean; //반지하
+  };
+  option: {
+    elevator: boolean;
+    park: boolean;
+    cctv: boolean;
+    doorLock: boolean;
+    pet: boolean;
+    veranda: boolean;
+    range: "induction" | "gas" | false;
+    airConditioner: "top" | "wall" | "stand" | false;
+    refrigerator: boolean;
+    sink: boolean;
+    tv: boolean;
+    internet: boolean;
+    bed: boolean;
+    desk: boolean;
+    microwave: boolean;
+    closet: boolean;
+    shoeRack: boolean;
+    bidet: boolean;
+    interphone: boolean;
+    parking: boolean;
+    security: boolean;
+    deilveryBox: boolean;
+    BuildingEntrance: boolean;
+    washingMachine: boolean;
+  };
+}
