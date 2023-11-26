a35a7800e68d1c851ec5cb0d08a5fa5aae44cefa
YOOJEHYEON <jehyunyoo@ajou.ac.kr>
2023-11-26 22:23:44 +0900

    css styl 적용

--- app/component/board/BoardList.tsx
+++ app/component/board/BoardList.tsx
@@ -1,14 +1,28 @@
-"use client";
-
-import { house_test_data } from "@/app/component/fortest/house";
-import HousePreview from "./HousePreview";
+'use client';
+import { useState, useEffect } from 'react';
+import { house_test_data } from '@/app/component/fortest/house';
+import HousePreview from './HousePreview';
+import axios from 'axios';
 
 export default function BoardList() {
   // TODO: 정보 불러와서 집어넣기
+  const [houses, setHouses] = useState();
+
+  useEffect(() => {
+    const getHouseInfo = async () => {
+      try {
+        const res = await axios.get('');
+        setHouses(res.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getHouseInfo();
+  }, [houses]);
 
   return (
-    <section className="grid grid-cols-2 gap-y-8 w-full px-24">
-      {house_test_data.map((house) => (
+    <section className='grid grid-cols-2 gap-y-8 w-full px-24'>
+      {houses.map((house) => (
         <HousePreview key={house.id} house={house} />
       ))}
     </section>
